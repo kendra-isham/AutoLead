60637342ba8bad77f6765af8d2d0b9ab91060977
Kendra Isham Linux <khutslar@gmail.com>
2020-06-30 21:52:43 -0400

    getting ready for heroku

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const past = requuire("path");
 
 // parse on request bodies 
 app.use(express.json());
@@ -11,6 +12,14 @@ app.use(cors());
 // import routes
 const watsonRoutes = require("./routes/watson");
 app.use("/watson", watsonRoutes);
+
+// use for production
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    });
+}
 
 // start server
 const port = process.env.PORT || 5000;
